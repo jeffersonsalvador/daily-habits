3ced96776d51d0595d4aaa63051331a46d331a53
Jefferson Costa <jeffersonsalvador@gmail.com>
2023-03-12 22:20:27 +0100

    base structure

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+const app = Fastify()
+
+app.register(cors)
+app.get('/', () => {
+    return 'Hello world!'
+})
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('Server running')
+})
